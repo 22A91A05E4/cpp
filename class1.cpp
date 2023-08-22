ab33ffd402ab7d764e6bbc672fb9b7c46dd7ed29
22A91A05E4 <119839946+22A91A05E4@users.noreply.github.com>
2023-08-22 15:40:23 +0530

    Update class1.cpp

--- class1.cpp
+++ class1.cpp
@@ -2,29 +2,20 @@
 using namespace std;
 class Student{        // 22A91A05E4
     private:
-    	int rollno;
+    	int rollno;   // member variables
     	string name;
     public:
-//    int rollno;
-//    string name;
-    //int arr[5];
-    Student(int a,string n){
+    Student(int a,string n){   // member function
         rollno=a;
         name=n;
-    //    arr[0]=2;
     }
 	void display(){
 			cout << rollno << " " << name << endl;
 		}
-    // delete(){
-    //     arr.pop();
-    // }
-
 };
 
 int main()
 {
 	Student s1(1,"sahi");
 	s1.display();
-	
 }
